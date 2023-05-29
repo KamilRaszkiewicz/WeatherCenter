f0ccd8a2bea633718b244d7a8cc99a3685266c3e
KamilRaszkiewicz <Kamil.Raszkiewicz02@gmail.com>
2023-05-29 14:30:34 +0200

    Implemented services

---
+++ Services/WeatherapiAutocompleteService.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.WebUtilities;
+using System.Text.Json;
+using WeatherCenter.Dto.Autocomplete;
+using WeatherCenter.Interfaces;
+
+namespace WeatherCenter.Services
+{
+    public class WeatherapiAutocompleteService : IAutocompleteService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _apiKey;
+
+        public WeatherapiAutocompleteService(IHttpClientFactory clientFactory, IConfiguration configuration)
+        {
+            _httpClient = clientFactory.CreateClient("weatherapi.com");
+            _apiKey = configuration["ApiKeys:weatherapi.com"];
+        }
+
+        public async Task<IEnumerable<CityAutocompleteDto>> GetAutocompletes(string searchQuery)
+        {
+            var parameters = new Dictionary<string, string>()
+            {
+                {"key", _apiKey},
+                {"q", searchQuery }
+            };
+
+            var query = QueryHelpers.AddQueryString("v1/search.json", parameters);
+
+            var result = await _httpClient.GetAsync(query);
+
+            if (!result.IsSuccessStatusCode)
+            {
+                return Enumerable.Empty<CityAutocompleteDto>();
+            }
+
+            var deserializedResult = JsonSerializer.Deserialize<IEnumerable<WeatherapiAutocompleteResponseDto>>(await result.Content.ReadAsStringAsync());
+
+            if (deserializedResult == null || !deserializedResult.Any())
+            {
+                return Enumerable.Empty<CityAutocompleteDto>();
+            }
+
+            return deserializedResult.Select(x => new CityAutocompleteDto()
+            {
+                Name = x.name,
+                Region = x.region,
+                Country = x.country,
+                Latitude = x.lat,
+                Longtitude = x.lon,
+            });
+        }
+    }
+}
---
+++ Services/WeatherapiWeatherService.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.WebUtilities;
+using System.Text.Json;
+using WeatherCenter.Dto.Autocomplete;
+using WeatherCenter.Dto.Weather;
+using WeatherCenter.Dto.Weather.Weatherapi;
+using WeatherCenter.Interfaces;
+
+namespace WeatherCenter.Services
+{
+    public class WeatherapiWeatherService : IWeatherService
+    {
+
+        private readonly HttpClient _httpClient;
+        private readonly string _apiKey;
+
+        public WeatherapiWeatherService(IHttpClientFactory clientFactory, IConfiguration configuration)
+        {
+            _httpClient = clientFactory.CreateClient("weatherapi.com");
+            _apiKey = configuration["ApiKeys:weatherapi.com"];
+        }
+
+        public async Task<WeatherInfoDto> GetCurrentWeather(double latitude, double longitude)
+        {
+            var parameters = new Dictionary<string, string?>()
+            {
+                {"key", _apiKey},
+                {"q", latitude.ToString() + "," + longitude.ToString() }
+            };
+
+            var query = QueryHelpers.AddQueryString("v1/current.json", parameters);
+
+            var result = await _httpClient.GetAsync(query);
+
+            if (!result.IsSuccessStatusCode)
+            {
+                return new WeatherInfoDto() 
+                { 
+                    IsSuccess = false
+                };
+            }
+
+            var deserializedResult = JsonSerializer.Deserialize<WeatherapiCurrentResponse>(await result.Content.ReadAsStringAsync());
+
+            if (deserializedResult == null)
+            {
+                return new WeatherInfoDto()
+                {
+                    IsSuccess = false
+                };
+            }
+
+            return new WeatherInfoDto()
+            {
+                IsSuccess = true,
+
+                Temperature = deserializedResult.current.temp_c,
+                FeelsLikeTemperature = deserializedResult.current.feelslike_c,
+
+                WindSpeed = deserializedResult.current.wind_kph,
+                Humidity = deserializedResult.current.humidity,
+                CloudPercentage = deserializedResult.current.cloud,
+
+                LocalTime = deserializedResult.location.localtime,
+
+                SummaryIconUrl = deserializedResult.current.condition.icon
+            };
+
+        }
+    }
+}

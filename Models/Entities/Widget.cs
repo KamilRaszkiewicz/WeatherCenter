1a9d53ec372495e2dff634fbe68d4b4c3380e437
KamilRaszkiewicz <Kamil.Raszkiewicz02@gmail.com>
2023-06-02 14:34:17 +0200

    Little changes in model

--- Models/Entities/Widget.cs
+++ Models/Entities/Widget.cs
@@ -9,7 +9,12 @@
         public string Country { get; set; }
         public string? Region { get; set; }
 
+        public string FullLocation { get; set; }
+
+        public int UtcOffset { get; set; }
         public double Latitude { get; set; }
         public double Longtitude { get; set; }
+
+        public DateTime CreatedAt { get; set; }
     }
 }
